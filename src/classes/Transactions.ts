dcd5bd903fca86f14625e965ab839fc07884870c
naftalimurgor <murgornaftali@gmail.com>
2023-11-18 13:36:20 +0300

    feat: add transactions wrapper

---
+++ src/classes/Transactions.ts
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import { AddressTransaction, SDKConfig } from '../types'
+
+export class Transaction {
+  private apiV1URL: string
+  private logger: (arg: string) => void
+  constructor(config: SDKConfig) {
+    this.apiV1URL = config.baseAPIURL || 'https://api.bitaps.com/bgl/v1/blockchain'
+    this.logger = this.logger || ((arg) => arg)
+  }
+
+  /**
+   * @param txHash transaction hash of the the transaction to fetch
+   * getTransactionByHash fetches a transaction by its hash
+   */
+  public async getTransactionByHash(txHash: string) {
+    const url = `${this.apiV1URL}/transaction/${txHash}`
+    const data = await this._get(url)
+    return data as AddressTransaction
+  }
+
+  /**
+   * @param txHash
+   * getTransactonMerkelProof fetches the MerkelProof of a transaction
+   */
+  public getTransactonMerkelProof(txHash: string) {
+
+  }
+
+  /// BEGIN PRIVATE METHODS
+  private async _get(url: string) {
+    try {
+      const res = await axios.get(`${this.apiV1URL}/${url}`)
+      const { data } = res.data
+      return data
+    } catch (error) {
+      this.logger(error)
+      return
+    }
+  }
+}
