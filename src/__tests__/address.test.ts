bbc79fbbf59d4f4075d6ebf11f939ed37188c6a2
naftalimurgor <murgornaftali@gmail.com>
2023-11-18 17:08:04 +0300

    test: add tests for address wrapper

---
+++ src/__tests__/address.test.ts
@@ -0,0 +1,38 @@
+import { BitgesellBlockchainSDK } from "../api"
+import { sdkConfig } from "./sdkConfig"
+
+describe('Address Wrapper class tests', () => {
+  let bitgesellBlockchainSDK: BitgesellBlockchainSDK
+  const bitgesellAddress = 'bgl1qlmzckh904vze03n0lwzptt5dkmvf2vj3ev4qm9'
+
+  beforeAll(() => {
+    bitgesellBlockchainSDK = new BitgesellBlockchainSDK(sdkConfig)
+  })
+
+  it('should fetch address balance', async () => {
+    try {
+      const addressBalance = await bitgesellBlockchainSDK.address.getAddressBalance(bitgesellAddress)
+      expect(addressBalance).toBeDefined()
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+  it('should get address transactions', async () => {
+    const addressTxes = await bitgesellBlockchainSDK.address.getAddressTransactions(bitgesellAddress)
+    expect(addressTxes).toBeDefined()
+  })
+
+  it('should get address UTXO', async () => {
+    const addressUTXOs = await bitgesellBlockchainSDK.address.getAddressUTXO(bitgesellAddress)
+    expect(addressUTXOs).toBeDefined()
+  })
+
+  it('should get address transactions', async () => {
+    const addressTxes = await bitgesellBlockchainSDK.address.getUnconfirmedAddressTransactions(bitgesellAddress)
+    expect(addressTxes).toBeDefined()
+  })
+
+
+
+})
