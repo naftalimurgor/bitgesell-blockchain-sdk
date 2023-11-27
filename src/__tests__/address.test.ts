8a76c1c0977531c312be320d82b96e93f9699a7a
naftalimurgor <murgornaftali@gmail.com>
2023-11-27 20:34:01 +0300

    test: fix Address tests

--- src/__tests__/address.test.ts
+++ src/__tests__/address.test.ts
@@ -1,7 +1,12 @@
 import { BitgesellBlockchainSDK } from "../api"
-import { sdkConfig } from "./sdkConfig"
+// import { sdkConfig } from "./sdkConfig"
 
-describe('Address Wrapper class tests', () => {
+describe.only('Address Wrapper class tests', () => {
+  const sdkConfig = {
+    baseAPIURL: 'https://api.bitaps.com/bgl/v1/blockchain',
+    logger: console.log
+  }
+
   let bitgesellBlockchainSDK: BitgesellBlockchainSDK
   const bitgesellAddress = 'bgl1qlmzckh904vze03n0lwzptt5dkmvf2vj3ev4qm9'
 
@@ -12,6 +17,7 @@ describe('Address Wrapper class tests', () => {
   it('should fetch address balance', async () => {
     try {
       const addressBalance = await bitgesellBlockchainSDK.address.getAddressBalance(bitgesellAddress)
+      console.log(addressBalance)
       expect(addressBalance).toBeDefined()
     } catch (error) {
       console.log(error)
@@ -28,11 +34,10 @@ describe('Address Wrapper class tests', () => {
     expect(addressUTXOs).toBeDefined()
   })
 
-  it('should get address transactions', async () => {
+  it('should get address unconfirmed transactions', async () => {
     const addressTxes = await bitgesellBlockchainSDK.address.getUnconfirmedAddressTransactions(bitgesellAddress)
     expect(addressTxes).toBeDefined()
   })
-
 
 
 })
