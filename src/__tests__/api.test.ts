5220e5297a9eda481d5b4e902e0d6641379ad28e
naftalimurgor <murgornaftali@gmail.com>
2023-11-27 23:55:21 +0300

    test: add `BlockchainSDK` class tests

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,5 +1,16 @@
+import { Address, BitgesellBlockchainSDK, Blockchain, Mempool, Transaction } from "../api"
+import { sdkConfig } from "./sdkConfig"
+
 describe('API tests', () => {
-    it('should fetch mempool state', () => {
+    let bitgesellBlockchainSDK: BitgesellBlockchainSDK
+
+    it('should instantiate BitgesellBlockchainSDK correctly', () => {
+
+        bitgesellBlockchainSDK = new BitgesellBlockchainSDK(sdkConfig)
+        expect(bitgesellBlockchainSDK.tx).toBeInstanceOf(Transaction)
+        expect(bitgesellBlockchainSDK.mempool).toBeInstanceOf(Mempool)
+        expect(bitgesellBlockchainSDK.address).toBeInstanceOf(Address)
+        expect(bitgesellBlockchainSDK.blockchain).toBeInstanceOf(Blockchain)
 
     })
 })
